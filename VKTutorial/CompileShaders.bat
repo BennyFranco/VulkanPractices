09776ce0771f79f3dfea87515c2fe9b6912179f7
Benny Franco <bennyfranco@icloud.com>
2019-06-10 15:08:40 -0700

    Depth buffering

--- VKTutorial/CompileShaders.bat
+++ VKTutorial/CompileShaders.bat
@@ -1,4 +1,4 @@
-C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./HelloTriangle/shaders/shader.vert -o ./HelloTriangle/shaders/vert.spv
-C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./HelloTriangle/shaders/shader.frag -o ./HelloTriangle/shaders/frag.spv
+C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./VKTutorial/shaders/shader.vert -o ./VKTutorial/shaders/vert.spv
+C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./VKTutorial/shaders/shader.frag -o ./VKTutorial/shaders/frag.spv
 
 pause
