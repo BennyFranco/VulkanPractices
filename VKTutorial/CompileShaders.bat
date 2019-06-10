852e1386e74f37e450f9d4bd8532e4e63488bb4f
Benny Franco <bennyfranco@icloud.com>
2019-06-10 13:06:23 -0700

    Initial commit

---
+++ VKTutorial/CompileShaders.bat
@@ -0,0 +1,4 @@
+C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./HelloTriangle/shaders/shader.vert -o ./HelloTriangle/shaders/vert.spv
+C:/VulkanSDK/1.1.106.0/Bin/glslangValidator.exe -V ./HelloTriangle/shaders/shader.frag -o ./HelloTriangle/shaders/frag.spv
+
+pause
